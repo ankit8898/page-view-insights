ee571cb26efb4901e30427ac11d3ea6182f1be7b
Ankit Gupta <ankit.gupta8898@gmail.com>
2017-09-13 15:52:14 -0700

    Using blank? check for url

--- server/app/models/concerns/url_validator.rb
+++ server/app/models/concerns/url_validator.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
 module UrlValidator
+  #Utility class for url validation
 
+  # Checks if the url is valid
   def valid_url?(url)
     url.include?('http://') || url.include?('https://') || url.include?('www.')
   end
--- server/app/models/page_view.rb
+++ server/app/models/page_view.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
 class PageView < Sequel::Model
+  # Page view model holds information about page_views. Urls visited, referrer
+  # to those urls
 
   include UrlValidator
 
@@ -7,7 +10,7 @@ class PageView < Sequel::Model
   # url: should have valid format
   def validate
     super
-    errors.add(:url, 'cannot be empty') if !url || url.empty?
+    errors.add(:url, 'cannot be empty') if url.blank?
     errors.add(:url, 'is not a valid URL') unless valid_url?(url) #url =~ /\Ahttps?:\/\//
     errors.add(:referrer, 'is not a valid URL') unless valid_url?(referrer)
   end
